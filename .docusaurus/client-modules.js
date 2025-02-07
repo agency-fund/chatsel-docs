3c38aaea36ae1219c1d3db309f353b063feaaad6
Zezhen Wu <michael@agency.fund>
2025-02-07 14:50:04 -0500

    Deploy website - based on 42125955ac21efde65b33497020b4684c8058dcf

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/michaelfive/Code/TAF_repo/chatsel-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/michaelfive/Code/TAF_repo/chatsel-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/michaelfive/Code/TAF_repo/chatsel-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/michaelfive/Code/TAF_repo/chatsel-docs/src/css/custom.css"),
-];
